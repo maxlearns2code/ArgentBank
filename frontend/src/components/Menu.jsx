3973d0b50018d6132a3ddfafc606655948e03796
Maxime <maxdevvynter@gmail.com>
2024-01-30 10:48:33 +0100

    Add route configuration to the project + add main nav

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,22 @@
+import HeaderLogo from "../assets/argentBankLogo.png"
+
+const Menu = () => (
+  <nav className="main-nav">
+    <a className="main-nav-logo" href="/">
+      <img
+        className="main-nav-logo-image"
+        src={HeaderLogo}
+        alt="Argent Bank Logo"
+      />
+      <h1 className="sr-only">Argent Bank</h1>
+    </a>
+    <div>
+      <a className="main-nav-item" href="./sign-in.html">
+        <i className="fa fa-user-circle"></i>
+        Sign In
+      </a>
+    </div>
+  </nav>
+);
+
+export default Menu;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./main.scss";
+import Root from "./routes/root";
+import ErrorPage from "./pages/error-page";
+import Home/*, { loader as homeLoader } */from "./pages/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        /*loader: homeLoader,*/
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
