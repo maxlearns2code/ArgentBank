59b1018a1d9e38840d2c0764fd9bfe9799fdd0da
Maxime <maxdevvynter@gmail.com>
2024-02-05 12:03:38 +0100

    add authentification + redirection

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,3 +1,44 @@
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  redirection();
+};
+
+// add function redirection by API
+async function redirection() {
+  let baliseUsername = document.getElementById("username");
+  let emailValue = baliseUsername.value;
+
+  let balisePassword = document.getElementById("password");
+  let passwordValue = balisePassword.value;
+
+  // add fetch API to validate authentification
+  const response = await fetch("http://localhost:3001/api/v1/user/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: emailValue,
+      password: passwordValue,
+    }),
+  });
+
+  if (response.ok) {
+    //save token
+    const result = await response.json();
+    sessionStorage.setItem('authToken', result.token);
+    //redirection
+    window.location = "/user";
+  } else {
+    //alert
+    const alert = document.querySelector(".alert");
+    alert.style.display = null;
+    const alertBtn = document.querySelector(".alertBtn");
+    alertBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.location = "/signin";
+    });
+  }
+}
+
 const Form = () => {
   return (
     <form>
@@ -13,7 +54,9 @@ const Form = () => {
         <input type="checkbox" id="remember-me" />
         <label htmlFor="remember-me">Remember me</label>
       </div>
-      <button className="sign-in-button">Sign In</button>
+      <button className="sign-in-button" onClick={handleSubmit}>
+        Sign In
+      </button>
     </form>
   );
 };
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -6,6 +6,10 @@ const Signin = () => {
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
+        <div className="alert" style={{ display: "none" }}>
+          <p>Erreur dans l’identifiant ou le mot de passe</p>
+          <button className="alert__btn">OK</button>
+        </div>
         <Form />
       </section>
     </main>
