b4f2d0aee54ea968d77503201ff2d0c4d5d2c083
Maxime <maxdevvynter@gmail.com>
2024-02-13 11:08:03 +0100

    fix login and logout

--- frontend/src/layout/Menu.jsx
+++ frontend/src/layout/Menu.jsx
@@ -1,11 +1,13 @@
-import LoginBtn from "../components/LoginBtn";
-import LogoutBtn from "../components/LogoutBtn";
 import HeaderLogo from "/src/assets/argentBankLogo.png";
 import { Link } from "react-router-dom";
 import {useSelector} from "react-redux";
 
 const Menu = () => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const handleLogout = () => {
+    sessionStorage.removeItem("authToken")
+    isLoggedIn(false);
+  };
 
   return (
     <nav className="main-nav">
@@ -18,9 +20,23 @@ const Menu = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
       {isLoggedIn ? (
-          <LogoutBtn />
+        <div>
+        <Link className="main-nav__item" to="/user">
+          <i className="fa fa-user-circle"></i>
+          &nbsp;Tony
+        </Link>
+        <Link className="main-nav__item" to="/" onClick={handleLogout}>
+          <i className="fa fa-sign-out"></i>
+          &nbsp;Sign Out
+        </Link>
+      </div>
         ) : (
-          <LoginBtn />
+          <div>
+      <Link className="main-nav__item" to="/signin">
+        <i className="fa fa-user-circle"></i>
+        &nbsp;Sign In
+      </Link>
+    </div>
         )}
       
       
